3f30bcad2778e266862300425d55b1f2d899b9dd
Elvinas Stanionis <elvinas.stanionis@scoding.com>
2022-09-23 13:18:32 +0300

    TW-26 moving docker container registry to different path.

--- push.sh
+++ push.sh
@@ -2,29 +2,38 @@
 
 REGISTRY_URL='registry.gitlab.com/sc-rep/scoding/internal7/docker-images'
 
-docker build -t $REGISTRY_URL/app_elasticsearch:8.0 app_elasticsearch/8.0/.
-docker push $REGISTRY_URL/app_elasticsearch:8.0
+#docker build -t $REGISTRY_URL/app_elasticsearch:8.0 app_elasticsearch/8.0/.
+#docker push $REGISTRY_URL/app_elasticsearch:8.0
+#
+#docker build -t $REGISTRY_URL/app_kibana:8.0 app_kibana/8.0/.
+#docker push $REGISTRY_URL/app_kibana:8.0
+#
+#docker build -t $REGISTRY_URL/app_mailhog:latest app_mailhog/latest/.
+#docker push $REGISTRY_URL/app_mailhog:latest
+#
+#docker build -t $REGISTRY_URL/app_mariadb:latest app_mariadb/latest/.
+#docker push $REGISTRY_URL/app_mariadb:latest
+#
+#docker build -t $REGISTRY_URL/app_nginx:latest app_nginx/latest/.
+#docker push $REGISTRY_URL/app_nginx:latest
+#
+#docker build -t $REGISTRY_URL/app_php:8.1 app_php/8.1/.
+#docker push $REGISTRY_URL/app_php:8.1
+#
+#docker build -t $REGISTRY_URL/app_php_xdebug:8.1 app_php_xdebug/8.1/.
+#docker push $REGISTRY_URL/app_php_xdebug:8.1
+#
+#docker build -t $REGISTRY_URL/app_rabbitmq:latest app_rabbitmq/latest/.
+#docker push $REGISTRY_URL/app_rabbitmq:latest
+#
+#docker build -t $REGISTRY_URL/app_redis:latest app_redis/latest/.
+#docker push $REGISTRY_URL/app_redis:latest
 
-docker build -t $REGISTRY_URL/app_kibana:8.0 app_kibana/8.0/.
-docker push $REGISTRY_URL/app_kibana:8.0
+docker build -t $REGISTRY_URL/etaplius_elasticsearch:8.4.1 etaplius_elasticsearch/8.4.1/.
+docker push $REGISTRY_URL/etaplius_elasticsearch:8.4.1
 
-docker build -t $REGISTRY_URL/app_mailhog:latest app_mailhog/latest/.
-docker push $REGISTRY_URL/app_mailhog:latest
+docker build -t $REGISTRY_URL/etaplius_kibana:8.4.1 etaplius_kibana/8.4.1/.
+docker push $REGISTRY_URL/etaplius_kibana:8.4.1
 
-docker build -t $REGISTRY_URL/app_mariadb:latest app_mariadb/latest/.
-docker push $REGISTRY_URL/app_mariadb:latest
-
-docker build -t $REGISTRY_URL/app_nginx:latest app_nginx/latest/.
-docker push $REGISTRY_URL/app_nginx:latest
-
-docker build -t $REGISTRY_URL/app_php:8.1 app_php/8.1/.
-docker push $REGISTRY_URL/app_php:8.1
-
-docker build -t $REGISTRY_URL/app_php_xdebug:8.1 app_php_xdebug/8.1/.
-docker push $REGISTRY_URL/app_php_xdebug:8.1
-
-docker build -t $REGISTRY_URL/app_rabbitmq:latest app_rabbitmq/latest/.
-docker push $REGISTRY_URL/app_rabbitmq:latest
-
-docker build -t $REGISTRY_URL/app_redis:latest app_redis/latest/.
-docker push $REGISTRY_URL/app_redis:latest
+docker build -t $REGISTRY_URL/etaplius_php:8.1 etaplius_php/8.1/.
+docker push $REGISTRY_URL/etaplius_php:8.1
